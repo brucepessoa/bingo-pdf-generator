e3da0d9890ebc7b3fb2b37758e37baf7c62f7b6a
Bruce Pessoa <102379493+brucepessoa@users.noreply.github.com>
2025-08-19 15:54:33 -0300

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { renderToStream } from "@react-pdf/renderer";
-import { BingoPDF } from "../../components/BingoPDF";
+import BingoPDF from "../../components/BingoPDF"; // Importação padrão
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
